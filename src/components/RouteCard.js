84352b7cfb13463789fdc2f02db9a271cdf0e65c
Larry Anderson <larryboymi@hotmail.com>
2017-01-25 15:22:57 -0600

    add modal

--- src/components/RouteCard.js
+++ src/components/RouteCard.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 import Avatar from 'material-ui/Avatar'
+import Dialog from 'material-ui/Dialog'
+
 import {
   indigo900,
   deepOrange300
@@ -17,28 +19,72 @@ const styles = {
   }
 }
 
-const RouteCard = ({route: {ID, Description, TargetPort, ProxiedURL}}) => (
-    <Card zDepth={1} style={styles.card}>
-      <CardHeader
-        title={ID}
-        subtitle={Description || '<no description>'}
-        avatar={
-          <Avatar
-            color={deepOrange300}
-            backgroundColor={indigo900}
-            size={30}
-            style={styles.avatar}>{ID.charAt(0).toUpperCase()}</Avatar>
-        }
+class RouteCard extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      editable: false
+    }
+  }
+
+  handleEdit = () => {
+    this.setState({editable: true})
+  }
+
+  handleDoneEdit = () => {
+    this.setState({editable: false})
+  }
+
+  render () {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleDoneEdit}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleDoneEdit}
       />
-      <CardTitle title='Proxied URL' subtitle={ProxiedURL || '<none provided>'} />
-      <CardText>
-        Some other routey stuff.
-      </CardText>
-      <CardActions>
-        <FlatButton label="Edit" />
-      </CardActions>
-    </Card>
-)
+    ]
+
+    const {route: {ID, Description, TargetPort, ProxiedURL}} = this.props
+    return (
+      <Card zDepth={1} style={styles.card}>
+        <CardHeader
+          title={ID + ':' + TargetPort}
+          subtitle={Description || '<no description>'}
+          avatar={
+            <Avatar
+              color={deepOrange300}
+              backgroundColor={indigo900}
+              size={30}
+              style={styles.avatar}>{ID.charAt(0).toUpperCase()}</Avatar>
+          }
+        />
+        <CardTitle title='Proxied URL' subtitle={ProxiedURL || '<none provided>'} />
+        <CardText>
+          Some other routey stuff.
+        </CardText>
+        <CardActions>
+          <FlatButton label="Edit" onTouchTap={this.handleEdit}/>
+          <Dialog
+            title={'Edit Route (' + ID + ')'}
+            actions={actions}
+            modal={false}
+            open={this.state.editable}
+            onRequestClose={this.handleDoneEdit}
+          >
+            Would you like to change the proxied URL?
+          </Dialog>
+        </CardActions>
+      </Card>
+    )
+  }
+}
 
 RouteCard.displayName = 'RouteCard'
 
